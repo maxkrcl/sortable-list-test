2bbf51f5725471b14d2238be5b47e2c30c9118d7
maxkrcl <138573266+maxkrcl@users.noreply.github.com>
2023-09-15 09:25:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+const sortableList = document.querySelector('.sortable-list');
+const sortableItems = document.querySelectorAll('.sortable-list li');
 
+let sortableItem;
+let sortableIcon;
+
+sortableItems.forEach(item => {
+
+    const dragIndicator = item.querySelector('img');
+
+    dragIndicator.addEventListener('dragstart', () => {
+        sortableItem = item;
+        sortableIcon = dragIndicator;
+    });
+
+    dragIndicator.addEventListener('dragover', () => {
+        const sortableIconRect = sortableIcon.getBoundingClientRect();
+        const dragIndicatorRect = dragIndicator.getBoundingClientRect();
+        if (sortableIconRect.top > dragIndicatorRect.top) {
+            sortableList.insertBefore(sortableItem, item);
+        } else {
+            sortableList.insertBefore(item, sortableItem);
+        }
+    });
+
+});
